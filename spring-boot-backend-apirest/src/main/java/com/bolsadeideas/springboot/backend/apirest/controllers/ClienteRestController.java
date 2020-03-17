d5aeb12c6806704b600928859ef8925db7aae90e
C r a k <55041603+iCrak@users.noreply.github.com>
2020-03-17 17:24:20 -0500

    Se termino hasta las validaciones parte Backend y Frontend.

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,9 +1,20 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
+
+import java.util.HashMap;
+
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +23,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.backend.apirest.entity.Cliente;
@@ -33,31 +43,106 @@ public class ClienteRestController {
 	
 	@GetMapping("/clientes/{id}")
 	//@ResponseStatus(HttpStatus.OK) retorna 200
-	public Cliente show(@PathVariable Long id) {
-		return clienteservice.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Cliente cliente=null;
+		Map<String,Object> response=new HashMap<>();
+		try {
+			cliente = clienteservice.findById(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje","Error al realizar la consulta en la consulta en la base de datos");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if(cliente ==null) {
+			response.put("mensaje","El cliente ID: " .concat(id.toString().concat(" no existe en la base de datos!")));
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}		
+		return new ResponseEntity<Cliente>(cliente,HttpStatus.OK); 
 	}
 	
 	@PostMapping("/clientes")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente create(@RequestBody Cliente cliente) {
-		return clienteservice.save(cliente);
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente,BindingResult result) {		
+		Cliente clientNew = null;
+		Map<String,Object> response=new HashMap<>();
+		
+		/*if (result.hasErrors()) {
+			
+			List<String> errors=new ArrayList<>();			
+		for (FieldError	 err: result.getFieldErrors()) {
+			errors.add("El campo "+err.getField() + err.getDefaultMessage());
+			}
+			response.put("errors",errors);
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}*/
+		if (result.hasErrors()) {
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo "+ err.getField() +" " +err.getDefaultMessage())
+					.collect(Collectors.toList());
+						response.put("errors",errors);
+						return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+				
+		try {
+			clientNew = clienteservice.save(cliente);
+		} catch (DataAccessException e) {
+			response.put("mensaje","Error al realizar el insert en la base de datos");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El cliente ha sido creado con exito!");
+		response.put("cliente", clientNew);
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/clientes/{id}")
-	public Cliente update(@RequestBody Cliente cliente,@PathVariable Long id) {
-		Cliente clienteactual= clienteservice.findById(id);
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente,BindingResult result,@PathVariable Long id) {
+		Cliente clienteactual = clienteservice.findById(id);
 		
-		clienteactual.setNombre(cliente.getNombre());
-		clienteactual.setApellido(cliente.getApellido());		
-		clienteactual.setEmail(cliente.getEmail());
+		Cliente clienteupdate = null;
 		
-		return clienteservice.save(clienteactual);
+		Map<String,Object> response=new HashMap<>();
+		
+		if (result.hasErrors()) {
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo "+ err.getField() +" " +err.getDefaultMessage())
+					.collect(Collectors.toList());
+						response.put("errors",errors);
+						return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		if(clienteactual ==null) {
+			response.put("mensaje","Error: No se pudo editar, el cliente ID: " .concat(id.toString().concat(" no existe en la base de datos!")));
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}		
+		try {
+			clienteactual.setNombre(cliente.getNombre());
+			clienteactual.setApellido(cliente.getApellido());		
+			clienteactual.setEmail(cliente.getEmail());
+			clienteactual.setCreaAt(cliente.getCreaAt());
+			clienteupdate = clienteservice.save(clienteactual);			
+		} catch (DataAccessException e) {
+			response.put("mensaje","Error al realizar el actualizar en la base de datos");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El cliente ha sido actualizado con exito!");
+		response.put("cliente", clienteupdate);
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable Long id) {
-		clienteservice.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Map<String,Object> response=new HashMap<>();
+		try {
+			clienteservice.delete(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje","Error al eliminar en la base de datos");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return  new  ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje","El cliente eliminado con exito!");
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
 	}
 	
 }
--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/entity/Cliente.java
@@ -12,6 +12,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -26,10 +29,17 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	//@Column(length = 250,nullable = true,unique = true)
+	@NotEmpty(message = "no puede estar vacio")
+	@Size(min=4,max=12 , message = "el tamaño tiene que estar entre 4 y 12")
+	@Column(nullable = false)
 	private String nombre;
 	
+	@NotEmpty(message = "no puede estar vacio")
 	private String apellido;
+	
+	@NotEmpty(message = "no puede estar vacio")
+	@Email(message = "no es una formato de correo")
+	@Column(nullable = false,unique = true)
 	private String email;
 	
 	@Column(name = "creat_at")
